8cb10f88f15dea7a236678d58d89ca8494989666
adamdoha <adamdoha@linecorp.com>
2021-03-26 23:57:50 +0900

    first commit

---
+++ matching-developers/src/main/java/com/naverblog/dawndevelopers/DawnDevelopersApplication.java
@@ -0,0 +1,13 @@
+package com.naverblog.dawndevelopers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawnDevelopersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawnDevelopersApplication.class, args);
+    }
+
+}
---
+++ matching-developers/src/test/java/com/naverblog/dawndevelopers/DawnDevelopersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naverblog.dawndevelopers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawnDevelopersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
